15e9f8b282d681f5dc0c23aa0fd3da7a0c47a6a3
Sebastian Wallis <j.sebastian.aw@gmail.com>
2024-09-16 18:46:08 -0500

    Commit

--- Taller_16_09/primeraCifra.hs
+++ Taller_16_09/primeraCifra.hs
@@ -5,7 +5,7 @@ module Main where
 	first :: [int] -> int
 	first [] = 0
 	first :: Integer -> Integer
-	first :: -1 < n && n < 10 n = n
+	first :: (-1 < n) && (n < 10 n) = n
 	first :: n < 10 n= n/10
 
 
--- Taller_16_09/ultimaCifra.hs
+++ Taller_16_09/ultimaCifra.hs
@@ -1,1 +1,7 @@
-main :: IO
+module Main where
+
+	main::IO
+    last :: [int] -> int
+    last :: -1 < n && n < 10 n = n
+    last :: n > 9 n = n % 10
+
