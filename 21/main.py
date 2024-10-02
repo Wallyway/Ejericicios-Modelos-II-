b8d2896489b1d0befb5b882260310d381520b310
Sebastian Wallis <j.sebastian.aw@gmail.com>
2024-10-02 12:59:58 -0500

    Created user input, Ace value decision implemented, prints of cards obtained, dealer function added and made some corrections with assignments

--- 21/main.py
+++ 21/main.py
@@ -19,16 +19,10 @@ def game(cards, result: int, vals):
         return cards[:1] + game(cards[1:], result+vals[cards[:1][0][1]], vals)
     return [result]
 
-def vals(key):
-    return {'A': 1,'2': 2, '3': 3, '4':4, '5':5, '6':6, '7':7, '8':9, '9':9, '10':10, 'J':10, 'Q':10, 'K': 10}[key]
     
 def pocker():
     return cards(["club", "diamonds", "hearts", "spades"], ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'])
 
-# Player and house are tuples
-# Retorno [player, dealer]
-# Player es la tupla ([cards], value)
-# TODO: hacer que el usuario decida si continuar, hacer que el dealer pare aleatoriamente. El que tenga el mayor puntaje (value) menor o igual a 21 es el que gana
 def match(player, dealer, cards):
     if player[1] < 21 and dealer[1] < 21:
         return match((player[0]+cards[:1], player[1]+vals(cards[0][1])), (dealer[0]+cards[1:2], dealer[1]+vals(cards[1][1])), cards[2:])
